622c76243484c8f519edea842d952707648f4808
Vlad Mirel <Vlad Mirel>
2016-03-24 17:08:09 +0200

    the two queries

---
+++ USQLApplication1/UseView.usql.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace USQLApplication1
+{
+    public static class Helpers
+    {
+        public static bool TryParseInt(string column)
+        {
+            int result;
+            return Int32.TryParse(column, out result);
+        }
+    }
+}
